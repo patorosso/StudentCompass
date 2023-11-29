92bfc5afc0f684862da867174e5ff38ae33a865d
patricio-rosso <patriciosimonrosso@gmail.com>
2023-11-29 20:43:08 -0300

    register service repository

---
+++ web-api/Api/Helpers/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Api.Contracts;
+using Api.Services;
+
+namespace Api.Helpers
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IStudentRepository, StudentRepository>();
+
+            return services;
+        }
+    }
+}
