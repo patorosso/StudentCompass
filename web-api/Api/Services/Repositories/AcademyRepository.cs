ce32e78030c90e8d44c17f9a4d3b6b22d7d385fd
patricio-rosso <patriciosimonrosso@gmail.com>
2023-11-30 19:40:42 -0300

    add academic repository

---
+++ web-api/Api/Services/Repositories/AcademyRepository.cs
@@ -0,0 +1,14 @@
+﻿using Api.Data.Models;
+using Api.Services.Contracts;
+
+namespace Api.Services.Repositories
+{
+    public class AcademyRepository : IAcademicRepository
+    {
+        public async Task<IEnumerable<Subject>> GetProgressOverview()
+        {
+            await Task.Delay(1000);
+            return new List<Subject>();
+        }
+    }
+}
