d1540b41949217f223080c9e5d47a616c8935b62
patricio-rosso <patriciosimonrosso@gmail.com>
2023-11-30 20:44:23 -0300

    add base repository class

---
+++ web-api/Api/Services/Repositories/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Api.Services.Repositories
+{
+    public class BaseRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected async Task<SqlConnection> CreateConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+
+    }
+}
