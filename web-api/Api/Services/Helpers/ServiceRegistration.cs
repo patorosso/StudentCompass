fac7d8dd3a2887dabe1ec7087042fca3114946c3
patricio-rosso <patriciosimonrosso@gmail.com>
2023-11-30 20:46:30 -0300

    change to scoped service

--- web-api/Api/Services/Helpers/ServiceRegistration.cs
+++ web-api/Api/Services/Helpers/ServiceRegistration.cs
@@ -8,7 +8,7 @@ namespace Api.Services.Helpers
         public static IServiceCollection AddRepositories(this IServiceCollection services)
         {
             services.AddScoped<IStudentRepository, StudentRepository>();
-            services.AddSingleton<IAcademicRepository, AcademyRepository>(); //singleton because ... ?
+            services.AddScoped<IAcademicRepository, AcademyRepository>(); //singleton ... ?
 
             return services;
         }
