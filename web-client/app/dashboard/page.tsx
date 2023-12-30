c6876870e651108290e274023fad09c343add5f6
patricio-rosso <patriciosimonrosso@gmail.com>
2023-12-29 21:56:05 -0300

    add components to dashboard route

--- web-client/app/dashboard/page.tsx
+++ web-client/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SubjectsList from "./components/SubjectsList";
+import Analytics from "./components/Analytics";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex h-full justify-evenly">
+      <SubjectsList />
+      <Analytics />
+    </div>
+  );
 };
 
 export default Dashboard;
