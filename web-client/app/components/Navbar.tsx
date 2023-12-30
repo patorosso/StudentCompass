fa2127bdf1b43671384a45062bd3a466eba9c133
patricio-rosso <patriciosimonrosso@gmail.com>
2023-12-29 21:53:43 -0300

    add padding to navbar on all screens

--- web-client/app/components/Navbar.tsx
+++ web-client/app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 h-20 bg-white/30 backdrop-blur-md  border-gray-400 border-b-[1px]">
-      <nav className="flex items-center justify-between max-w-6xl mx-auto py-4 lg:px-4 h-full">
+    <header className="fixed top-0 left-0 right-0 h-20 bg-white/30 backdrop-blur-md  border-gray-400 border-b-[1px] ">
+      <nav className="flex items-center justify-between max-w-6xl mx-auto py-4 px-4 h-full">
         <Link
           href="/"
           className="text-xl font-semibold text-gray-700 hover:text-white transition-colors"
