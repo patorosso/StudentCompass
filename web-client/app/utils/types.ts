69fcac26257c62b432485a1a604caf08e45ea9ab
patricio-rosso <patriciosimonrosso@gmail.com>
2023-12-31 18:38:01 -0300

    add subject interface

---
+++ web-client/app/utils/types.ts
@@ -0,0 +1,11 @@
+interface Subject {
+  code: number;
+  description: string;
+  grade: number;
+  weeklyHours: number;
+  yearLevel: number;
+  isOptional: boolean;
+  isElective: boolean;
+  isAnnual: boolean;
+  isAvailable: boolean;
+}
