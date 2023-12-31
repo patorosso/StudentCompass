b6957f464382046c73552fcc1104ceb2e3dce59e
patricio-rosso <patriciosimonrosso@gmail.com>
2023-12-31 15:27:41 -0300

    modify title and description

--- web-client/app/page.tsx
+++ web-client/app/page.tsx
@@ -5,10 +5,15 @@ export default function Home() {
   return (
     <section className="flex flex-col h-full justify-between">
       <div className="flex flex-col lg:flex-row items-center justify-center">
-        <div className="flex flex-col justify-center items-center px-10 w-full lg:w-3/5 text-center">
-          <h1 className="text-black text-5xl md:text-6xl lg:text-8xl font-extrabold">
-            Student Compass
-          </h1>
+        <div className="flex flex-col justify-center items-center px-10 w-full lg:w-3/5">
+          <div>
+            <div className="bg-clip-text text-transparent bg-gradient-to-r from-third from-5% to-primary to-90% text-4xl md:text-5xl lg:text-8xl font-extrabold">
+              student
+            </div>
+            <div className="bg-clip-text text-transparent bg-gradient-to-r from-third from-5% to-second to-70% text-5xl md:text-6xl lg:text-9xl font-extrabold">
+              Compass
+            </div>
+          </div>
           <p className="text-gray-600 text-xl md:text-2xl lg:text-3xl mt-4">
             Navigate your college journey
           </p>
@@ -18,7 +23,7 @@ export default function Home() {
         </div>
       </div>
       <div className="flex items-center justify-center pt-12 pb-16">
-        <p className="text-black text-2xl md:text-4xl lg:text-6xl font-extrabold">
+        <p className="text-black dark:text-gray-300 text-2xl md:text-4xl lg:text-6xl font-extrabold">
           Take a look at what we provide
         </p>
       </div>
