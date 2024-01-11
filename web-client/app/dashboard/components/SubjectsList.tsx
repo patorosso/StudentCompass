1407692c347361c33390ab925657a699d56fa641
pato-rosso <patriciosimonrosso@gmail.com>
2024-01-10 21:14:21 -0300

    add subject row render in subject list

--- web-client/app/dashboard/components/SubjectsList.tsx
+++ web-client/app/dashboard/components/SubjectsList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SubjectRow from "./SubjectRow";
 
 const SubjectsList = async () => {
   const res = await fetch(
@@ -9,7 +10,7 @@ const SubjectsList = async () => {
   return (
     <div>
       <div
-        className="bg-gray-400 dark:bg-surface-200 text-white shadow-xl rounded-lg p-4"
+        className="bg-gray-400 dark:bg-surface-200 dark:text-white shadow-xl rounded-lg p-4"
         style={{ width: 920 }}
       >
         <div
@@ -29,29 +30,7 @@ const SubjectsList = async () => {
             </thead>
             <tbody>
               {subjects.map((subject) => (
-                <tr
-                  key={subject.code}
-                  className="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600"
-                >
-                  <td className="border-b dark:border-gray-700 p-4">
-                    {subject.code}
-                  </td>
-                  <td className="border-b dark:border-gray-700 p-4">
-                    {subject.description}
-                  </td>
-                  <td className="border-b dark:border-gray-700 p-4">
-                    {subject.finalGrade ? subject.finalGrade : "-"}
-                  </td>
-                  <td className="border-b dark:border-gray-700 p-4">
-                    {subject.weeklyHours}
-                  </td>
-                  <td className="border-b dark:border-gray-700 p-4">
-                    {subject.isAvailable ? "Yes" : "No"}
-                  </td>
-                  <td className="border-b dark:border-gray-700 p-4">
-                    {subject.status}
-                  </td>
-                </tr>
+                <SubjectRow key={subject.code} subject={subject} />
               ))}
             </tbody>
           </table>
