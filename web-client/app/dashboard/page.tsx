8dfe78875c998c7491bd01a9f4f0c3d0d8a4c9e7
patricio-rosso <patriciosimonrosso@gmail.com>
2024-03-23 17:27:50 -0300

    misc ui design changes

--- web-client/app/dashboard/page.tsx
+++ web-client/app/dashboard/page.tsx
@@ -6,7 +6,9 @@ const Dashboard = () => {
   return (
     <div>
       <div className="flex items-center px-4 h-20 dark:bg-slate-900 bg-slate-300 opacity-90 border-gray-400 dark:border-gray-500 border-b-[1px]">
-        <p className="text-3xl dark:text-white">Career Overview</p>
+        <p className="text-3xl dark:text-white">
+          Ingeniería en Informática - Plan 2023
+        </p>
       </div>
       <div className="flex flex-col justify-center items-center s2ml:flex-row s2ml:justify-evenly mt-14">
         <SubjectsList />
