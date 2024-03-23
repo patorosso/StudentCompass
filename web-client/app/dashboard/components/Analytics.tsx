ae2c886041e2cb57353cde3e4c86a0b50c743cf1
patricio-rosso <patriciosimonrosso@gmail.com>
2024-03-23 00:43:56 -0300

    modify background colors from summary progress section

--- web-client/app/dashboard/components/Analytics.tsx
+++ web-client/app/dashboard/components/Analytics.tsx
@@ -5,15 +5,15 @@ const Analytics = () => {
   return (
     <div className="dark:text-white">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className=" dark:bg-surface-300 dark:bg-opacity-55 text-white shadow-xl rounded-lg p-4 md:col-span-3 opacity-90">
+        <div className=" bg-gray-500 bg-opacity-55 dark:bg-surface-300 dark:bg-opacity-55 text-white shadow-xl rounded-lg p-4 md:col-span-3">
           <div className="flex justify-center items-center px-8 py-4 space-x-16">
-            <div className="flex justify-center items-center text-2xl text-black bg-gradient-to-br from-white from-10% to-90% to-gray-400 w-24 h-24 rounded-full shadow-xl">
+            <div className="flex justify-center items-center text-2xl text-black bg-gradient-to-br from-white from-30% to-98% to-gray-400 w-24 h-24 rounded-full shadow-xl">
               52/63
             </div>
-            <div className="flex justify-center items-center text-black bg-gradient-to-br from-white from-10% to-90% to-gray-400 w-24 h-24 rounded-full shadow-xl">
+            <div className="flex justify-center items-center text-black bg-gradient-to-br from-white from-30% to-98% to-gray-400 w-24 h-24 rounded-full shadow-xl">
               <RadialProgress progress={83} />
             </div>
-            <div className="flex justify-center items-center text-2xl text-black bg-gradient-to-br from-white from-10% to-90% to-gray-400 w-24 h-24 rounded-full shadow-xl">
+            <div className="flex justify-center items-center text-2xl text-black bg-gradient-to-br from-white from-30% to-98% to-gray-400 w-24 h-24 rounded-full shadow-xl">
               7.9
             </div>
           </div>
@@ -29,13 +29,13 @@ const Analytics = () => {
             </div>
           </div>
         </div>
-        <div className="bg-gray-400 dark:bg-surface-200 text-white shadow-xl rounded-lg p-4 md:col-span-2 h-40">
+        <div className="bg-gray-500 bg-opacity-55 dark:bg-surface-300 dark:bg-opacity-55 text-white shadow-xl rounded-lg p-4 md:col-span-2 h-40">
           Second
         </div>
-        <div className="bg-gray-400 dark:bg-surface-200 text-white shadow-xl rounded-lg p-4 md:col-span-1">
+        <div className="bg-gray-500 bg-opacity-55 dark:bg-surface-300 dark:bg-opacity-55 text-white shadow-xl rounded-lg p-4 md:col-span-1">
           Third
         </div>
-        <div className="bg-gray-400 dark:bg-surface-200 text-white shadow-xl rounded-lg p-4 md:col-span-3 h-32">
+        <div className="bg-gray-500 bg-opacity-55 dark:bg-surface-300 dark:bg-opacity-55 text-white shadow-xl rounded-lg p-4 md:col-span-3 h-32">
           Fourth
         </div>
       </div>
