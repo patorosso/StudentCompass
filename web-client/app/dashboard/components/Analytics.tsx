0f85bdb556ac35d86df8082d6ef9fef22354a845
patricio-rosso <patriciosimonrosso@gmail.com>
2024-03-23 00:07:55 -0300

    improve ui design for analytics

--- web-client/app/dashboard/components/Analytics.tsx
+++ web-client/app/dashboard/components/Analytics.tsx
@@ -5,7 +5,7 @@ const Analytics = () => {
   return (
     <div className="dark:text-white">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="bg-gradient-to-br from-indigo-900 from-20% to-pink-500 to-80% text-white shadow-xl rounded-lg p-4 md:col-span-3 opacity-90">
+        <div className=" dark:bg-surface-300 dark:bg-opacity-55 text-white shadow-xl rounded-lg p-4 md:col-span-3 opacity-90">
           <div className="flex justify-center items-center px-8 py-4 space-x-16">
             <div className="flex justify-center items-center text-2xl text-black bg-gradient-to-br from-white from-10% to-90% to-gray-400 w-24 h-24 rounded-full shadow-xl">
               52/63
@@ -18,13 +18,13 @@ const Analytics = () => {
             </div>
           </div>
           <div className="flex justify-center items-center space-x-16 text-white">
-            <div className="flex justify-center items-center bg-gray-300/30 backdrop-blur-md w-24 h-8 rounded-md shadow-xl">
+            <div className="flex justify-center items-center bg-primary w-24 h-8 rounded-md shadow-xl">
               Aprobadas
             </div>
-            <div className="flex justify-center items-center bg-gray-300/30 backdrop-blur-md w-24 h-8 rounded-md shadow-xl">
+            <div className="flex justify-center items-center bg-second w-24 h-8 rounded-md shadow-xl">
               Progreso
             </div>
-            <div className="flex justify-center items-center bg-gray-300/30 backdrop-blur-md w-24 h-8 rounded-md shadow-xl">
+            <div className="flex justify-center items-center bg-third backdrop-blur-md w-24 h-8 rounded-md shadow-xl">
               Promedio
             </div>
           </div>
