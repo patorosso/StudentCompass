1bd6ea8d828824586e8d6a6926b076050e4f130c
patricio-rosso <patriciosimonrosso@gmail.com>
2024-03-28 18:25:19 -0300

    add helpers

---
+++ web-client/app/utils/helpers.tsx
@@ -0,0 +1,16 @@
+export function joinClassNames(...classNames: string[]) {
+  return classNames.filter(Boolean).join(" ");
+}
+
+export function getStatusStyle(status: string) {
+  switch (status) {
+    case "Aprobada":
+      return "bg-green-600";
+    case "Disponible":
+      return "bg-yellow-500";
+    case "No disponible":
+      return "bg-red-500";
+    default:
+      return "bg-gray-400";
+  }
+}
