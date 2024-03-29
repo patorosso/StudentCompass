502ff38caa7603eb36e18b08c28f0f85ed3be95b
patricio-rosso <patriciosimonrosso@gmail.com>
2024-03-29 17:19:09 -0300

    modify base logger to generic logger type

--- web-api/Api/Services/Repositories/BaseRepository.cs
+++ web-api/Api/Services/Repositories/BaseRepository.cs
@@ -2,21 +2,32 @@
 
 namespace Api.Services.Repositories
 {
-    public class BaseRepository
+    public class BaseRepository<T>
     {
         private readonly IConfiguration _configuration;
+        private readonly ILogger<T> _logger;
 
-        public BaseRepository(IConfiguration configuration)
+        public BaseRepository(IConfiguration configuration, ILogger<T> logger)
         {
             _configuration = configuration;
+            _logger = logger;
         }
 
-        protected async Task<SqlConnection> CreateConnection()
+        protected async Task<SqlConnection?> CreateConnection()
         {
-            var connectionString = _configuration.GetConnectionString("DefaultConnection");
-            var connection = new SqlConnection(connectionString);
-            await connection.OpenAsync();
-            return connection;
+            try
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                var connection = new SqlConnection(connectionString);
+                await connection.OpenAsync();
+                _logger.LogInformation("Connection opened");
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to open a connection with the DB. Details: " + ex.Message);
+                return null;
+            }
         }
 
     }
