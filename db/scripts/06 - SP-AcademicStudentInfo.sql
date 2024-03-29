eeb901e736606ad3311cda79ff5eaa810bc9f169
patricio-rosso <patriciosimonrosso@gmail.com>
2024-03-28 22:27:16 -0300

    add exams as second return result

--- db/scripts/06 - SP-AcademicStudentInfo.sql
+++ db/scripts/06 - SP-AcademicStudentInfo.sql
@@ -8,14 +8,17 @@ CREATE OR ALTER PROCEDURE app.academic_student_info
 AS
 BEGIN
 
-DECLARE @approved_status_id BIT = 1;
+DECLARE @approved_status_id TINYINT = 1;
+DECLARE @in_progress_status_id TINYINT = 2;
+DECLARE @available_status_id TINYINT = 5;
+DECLARE @not_available_status_id TINYINT = 6;
 DECLARE @transversal_career_plan_id TINYINT = 0;
 
 WITH 
-approved_subjects AS ( -- get approved subjects of course table from the student and career params
-		SELECT subject_code, career_plan_id, status_id, final_grade 
+approved_or_in_progress_subjects AS ( -- get approved or in progress subjects of course table using SP params
+		SELECT subject_code, career_plan_id, status_id, final_grade, id 
 		FROM app.course c
-		WHERE student_id = @student_id AND status_id = @approved_status_id
+		WHERE student_id = @student_id AND status_id IN (@approved_status_id, @in_progress_status_id)
 		AND career_plan_id IN (@career_plan_id, @transversal_career_plan_id)
 ), 
 career_subjects AS ( -- get all subjects info from the career plan parameter and transversal ones
@@ -28,22 +31,25 @@ non_available_subjects AS ( -- get distinct non available subjects
 	ROW_NUMBER() OVER (PARTITION BY subject_code ORDER BY subject_code) as rn
 	FROM app.correlative 
 	WHERE subject_career_plan_id IN (@transversal_career_plan_id, @career_plan_id)
-	AND correlative_code NOT IN ( SELECT subject_code FROM approved_subjects )
+	AND correlative_code NOT IN ( SELECT subject_code FROM approved_or_in_progress_subjects )
 )
 
+-- first query: subjects list
 SELECT s.code, s.description, s.weekly_hours, s.year_level, s.is_optional,
-s.is_elective, s.is_annual, course_info.final_grade,
-CASE WHEN (j.rn = 1 OR is_elective = 1) THEN 6 -- '6 = No disponible'
-	 WHEN (status_id = 1) THEN 1 -- '1 = Aprobada'
-	 ELSE 5  -- '5 = Disponible'
+s.is_elective, s.is_annual, course_info.final_grade, course_info.id as course_id,
+CASE WHEN (j.rn = 1 OR is_elective = 1) THEN @not_available_status_id
+	 WHEN (status_id IN (@approved_status_id, @in_progress_status_id)) THEN status_id
+	 ELSE @available_status_id
 	 END AS [status]
-FROM approved_subjects course_info
+FROM approved_or_in_progress_subjects course_info
+RIGHT JOIN career_subjects s ON course_info.subject_code = s.code -- all subjects with their info
+LEFT JOIN non_available_subjects j ON j.rn = 1 AND j.no_disponible = s.code -- add available logic
 
--- all subjects with their info
-RIGHT JOIN career_subjects s ON course_info.subject_code = s.code
-
--- add available logic
-LEFT JOIN non_available_subjects j ON j.rn = 1 AND j.no_disponible = s.code
+-- second query: previous subjects grades
+SELECT ex.exam_id, ex.grade, ex.course_id
+FROM app.course_exam ex
+LEFT JOIN app.course c ON c.id = ex.course_id
+WHERE c.student_id = @student_id AND c.career_plan_id IN (@career_plan_id, @transversal_career_plan_id)
 
 END
 
