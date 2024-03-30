1290c73fbbe0502cfcabd9603457beda59027148
patricio-rosso <patriciosimonrosso@gmail.com>
2024-03-30 16:48:08 -0300

    modify return type and improve error msgs

--- db/scripts/09 - SP-CreateInProgressCourse.sql
+++ db/scripts/09 - SP-CreateInProgressCourse.sql
@@ -5,8 +5,7 @@ GO
 CREATE OR ALTER PROCEDURE app.create_in_progress_course
 @student_id SMALLINT,
 @career_plan_id TINYINT,
-@subject_code SMALLINT,
-@new_course_id INT OUTPUT
+@subject_code SMALLINT
 AS
 BEGIN
 
@@ -19,6 +18,7 @@ DECLARE @subject_exists BIT;
 DECLARE @student_exists BIT;
 DECLARE @is_available BIT;
 DECLARE @ok_previous_courses BIT;
+DECLARE @error_message NVARCHAR(150);
 	
 	-- Check subject
 	SELECT @subject_exists = CASE WHEN EXISTS(
@@ -26,8 +26,8 @@ DECLARE @ok_previous_courses BIT;
 		) THEN 1 ELSE 0 END;
 	IF @subject_exists = 0
 	BEGIN 
-        ;THROW 50001, 
-		'Subject does not exist or does not belong to the provided career plan.', 1;
+		SET @error_message = CONCAT('Subject with code ', CAST(@subject_code AS NVARCHAR), ' does not exist or does not belong to the provided career plan.');
+		;THROW 50001, @error_message, 1;
     END
 
 	-- Check student
@@ -37,8 +37,9 @@ DECLARE @ok_previous_courses BIT;
 		) THEN 1 ELSE 0 END;
 	IF @student_exists = 0
 	BEGIN 
-        ;THROW 50002, 
-		'Student does not exist or is not enrolled in the provided career plan.', 1;
+		SET @error_message = CONCAT('Student with ID = ', CAST(@student_id AS NVARCHAR), 
+		' does not exist or is not enrolled in the provided career plan with ID = ', CAST(@career_plan_id AS NVARCHAR));
+		;THROW 50002, @error_message, 1;
     END;
 
 	-- Check availability
@@ -56,8 +57,8 @@ DECLARE @ok_previous_courses BIT;
 		) THEN 1 ELSE 0 END;
 	IF @is_available = 0
 	BEGIN 
-        ;THROW 50003, 
-		'Subject currently not available for the student.', 1;
+		SET @error_message = CONCAT('Subject with code ', CAST(@subject_code AS NVARCHAR), ' is currently not available for the student.');
+		;THROW 50003, @error_message, 1;
     END
 
 	-- Check if previous courses taken only have status "Cursada" or "Desaprobada"
@@ -69,27 +70,27 @@ DECLARE @ok_previous_courses BIT;
 		) THEN 1 ELSE 0 END;
 	IF @ok_previous_courses = 0
 	BEGIN 
-        ;THROW 50000, 
-		'Course already taken or in progress.', 1;
+		SET @error_message = CONCAT('Subject with code ', CAST(@subject_code AS NVARCHAR), ' is already approved or in progress.');
+		;THROW 50004, @error_message, 1;
     END
 	
 	-- If all ok, insert
 	INSERT INTO app.course(student_id, subject_code, career_plan_id, status_id)
 	VALUES (@student_id, @subject_code, @career_plan_id, @in_progress_status_id);
 
-	SELECT @new_course_id = SCOPE_IDENTITY();
+	SELECT CAST(SCOPE_IDENTITY() AS INT) as course_id, @subject_code as subject_code
 END
 
 GO
 
 
-DECLARE @my_var INT;
---select * from app.course where student_id = 1
-EXEC app.create_in_progress_course 1,1,3624,@my_var out
+--DECLARE @my_var INT;
+----select * from app.course where student_id = 1
+--EXEC app.create_in_progress_course 1,1,3626
 
-SELECT @my_var
---student, career , subject
---select * from app.enrolled
+--SELECT @my_var
+----student, career , subject
+----select * from app.enrolled
 
-select * from app.course
+--select * from app.course
 
