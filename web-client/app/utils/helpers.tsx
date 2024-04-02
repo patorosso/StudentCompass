832bf98d575c636c9545507bc536c8aa4c485933
patricio-rosso <patriciosimonrosso@gmail.com>
2024-04-01 22:41:36 -0300

    add correlatives check logic

--- web-client/app/utils/helpers.tsx
+++ web-client/app/utils/helpers.tsx
@@ -16,3 +16,111 @@ export function getStatusStyle(status: string) {
       return "bg-gray-400";
   }
 }
+
+// quiza es mucha memoria pasar subjects tantas veces
+// mejora maxima: recortar el diccionario y buscar con valores "mayores" al codigo
+export const correlativeCheckConcat = (
+  correlatives: CorrelativesDict,
+  subjects: Subject[],
+  previousSubjectCode: number,
+  result: number[]
+) => {
+  // conseguir materias que necesitan tener a previousSubject aprobada
+  const correlativesKeys = Object.entries(correlatives)
+    .filter(([key, values]) => {
+      return values.includes(previousSubjectCode);
+    })
+    .map(([key]) => key);
+
+  let subjectsToWorry = correlativesKeys.filter((key) =>
+    subjects.find(
+      (subject) =>
+        subject.code === parseInt(key) &&
+        (subject.status === "Aprobada" ||
+          subject.status === "Disponible" ||
+          subject.status === "Cursando")
+    )
+  );
+
+  // este array va a tener las materias que necesito avisar que van a tener problemas
+  result.concat(subjectsToWorry.map((key) => parseInt(key)));
+
+  let correlativesToCheck = subjectsToWorry.filter((key) =>
+    subjects.find(
+      (subject) =>
+        subject.code === parseInt(key) && subject.status === "Aprobada"
+    )
+  );
+  correlativesToCheck.forEach((key) => {
+    correlativeCheckConcat(correlatives, subjects, parseInt(key), result);
+  });
+};
+
+// quiza es mucha memoria pasar subjects tantas veces
+// mejora maxima: recortar el diccionario y buscar con valores "mayores" al codigo
+export const correlativeCheck = (
+  correlatives: CorrelativesDict,
+  subjects: Subject[],
+  previousSubjectCode: number
+) => {
+  // conseguir materias que necesitan tener a previousSubject aprobada
+  const correlativesKeys = Object.entries(correlatives)
+    .filter(([key, values]) => {
+      return values.includes(previousSubjectCode);
+    })
+    .map(([key]) => key);
+
+  let subjectsToWorry = correlativesKeys.filter((key) =>
+    subjects.find(
+      (subject) =>
+        subject.code === parseInt(key) &&
+        (subject.status === "Aprobada" ||
+          subject.status === "Disponible" ||
+          subject.status === "Cursando")
+    )
+  );
+
+  let correlativesToCheck = subjectsToWorry.filter((key) =>
+    subjects.find(
+      (subject) =>
+        subject.code === parseInt(key) && subject.status === "Aprobada"
+    )
+  );
+  correlativesToCheck.forEach((key) => {
+    subjectsToWorry.concat(
+      correlativeCheck(correlatives, subjects, parseInt(key))
+    );
+  });
+
+  return subjectsToWorry;
+};
+
+export const getNewAvailableSubjects = (
+  subjectCode: number,
+  correlatives: CorrelativesDict,
+  subjects: Subject[]
+) => {
+  const correlativesSubjects = Object.entries(correlatives)
+    .filter(([key, values]) => {
+      return values.includes(subjectCode);
+    })
+    .map(
+      ([key]) =>
+        subjects.find((subject) => subject.code === parseInt(key)) as Subject
+    );
+
+  let result: Subject[] = [];
+
+  correlativesSubjects.forEach((subject) => {
+    const correlativesToCheckApproved = correlatives[subject.code];
+    const approved = correlativesToCheckApproved.every((correlative) => {
+      return subjects.find(
+        (subject) =>
+          subject.code === correlative && subject.status === "Aprobada"
+      );
+    });
+    if (approved) result.push(subject);
+  });
+
+  return result;
+};
