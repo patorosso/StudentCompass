75658cf759cecc8dda27227af1b9bb33ad3c83e4
patricio-rosso <patriciosimonrosso@gmail.com>
2024-04-06 01:10:33 -0300

    add handle save edit click

--- web-client/app/dashboard/progress/components/subjects/actions/SubjectsActions.tsx
+++ web-client/app/dashboard/progress/components/subjects/actions/SubjectsActions.tsx
@@ -4,15 +4,26 @@ import SubjectsMenu from "./SubjectsMenu";
 import EditSubject from "./EditSubject";
 import Image from "next/image";
 import { useSelector } from "react-redux";
-import { selectIsEditing, setEditingFalse } from "@/lib/features/subjectsSlice";
+import {
+  selectIsEditing,
+  setEditingFalse,
+  updateSubjects,
+  selectSubjectsToUpdate,
+} from "@/lib/features/subjectsSlice";
 import { useAppDispatch } from "@/lib/hooks";
 
 const SubjectsActions = () => {
   const dispatch = useAppDispatch();
   const isEditing = useSelector(selectIsEditing);
+  const subjectsToUpdate = useSelector(selectSubjectsToUpdate);
+  const isSaveDisabled = subjectsToUpdate.length === 0;
 
-  const onCancelClick = () => {
+  const onCancelEditClick = () => {
     dispatch(setEditingFalse());
+  };
+
+  const onSaveEditClick = () => {
+    dispatch(updateSubjects(subjectsToUpdate));
   };
 
   return (
@@ -24,14 +35,15 @@ const SubjectsActions = () => {
       >
         <button
           id="edit-button"
-          onClick={() => {}}
+          onClick={() => onSaveEditClick()}
+          disabled={isSaveDisabled}
           className="bg-success hover:bg-surface-400 text-gray-500 rounded-full w-9 h-9 flex items-center justify-center select-none mr-3"
         >
           <Image src="/save.svg" alt="Save edit" width={20} height={20} />
         </button>
         <button
           id="edit-button"
-          onClick={() => onCancelClick()}
+          onClick={() => onCancelEditClick()}
           className="bg-cancel hover:bg-surface-400 text-gray-500 rounded-full w-9 h-9 flex items-center justify-center select-none"
         >
           <Image src="/cancel.svg" alt="Cancel edit" width={20} height={20} />
