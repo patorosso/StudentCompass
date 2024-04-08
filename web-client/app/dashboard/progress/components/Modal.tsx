d4dcdcf198c7838777a93657fa13b92efe9ec7b1
patricio-rosso <patriciosimonrosso@gmail.com>
2024-04-07 23:37:04 -0300

    refactor modal

--- web-client/app/dashboard/progress/components/Modal.tsx
+++ web-client/app/dashboard/progress/components/Modal.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Image from "next/image";
 
 interface ModalProps {
-  title: string;
-  subtitle: string;
+  title?: string;
+  subtitle?: string;
   isOpen: boolean;
+  hasCancelIcon?: boolean;
+  hasCancelButton?: boolean;
   children: React.ReactNode;
+  width?: string;
+  height?: string;
   onClose: () => void;
   onAccept: () => void;
 }
 
 const Modal = ({
   title,
+  width,
+  height,
+  hasCancelIcon,
+  hasCancelButton,
   subtitle,
   isOpen,
   children,
@@ -21,25 +30,59 @@ const Modal = ({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center z-50">
-      <div className="bg-white dark:bg-slate-500 p-6 rounded-lg shadow-lg w-1/3">
-        <h2 className="text-3xl font-bold text-slate-600 dark:text-slate-200">
-          {title}
-        </h2>
-        <p className="my-6 text-slate-600 dark:text-slate-200">{subtitle}</p>
-        <div className="my-6">{children}</div>
-        <div className="flex justify-end space-x-2">
-          <button
-            onClick={onAccept}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Accept
-          </button>
-          <button
-            onClick={onClose}
-            className="px-4 py-2 bg-gray-600 rounded hover:bg-gray-400"
-          >
-            Exit
-          </button>
+      <div
+        className="flex flex-col bg-white dark:bg-surface-200 p-6 rounded-lg shadow-lg"
+        style={{ height, width }}
+      >
+        <div className="flex justify-between">
+          {title && (
+            <h2 className="text-xl font-bold text-slate-600 dark:text-slate-200">
+              {title}
+            </h2>
+          )}
+          {hasCancelIcon && (
+            <button onClick={onClose} className="select-none">
+              <Image
+                src="/cancel.svg"
+                alt="Close"
+                width={25}
+                height={25}
+                className="dark:hover:invert invert"
+              />
+            </button>
+          )}
+        </div>
+        {subtitle && (
+          <p className="my-6 text-slate-600 dark:text-slate-200">{subtitle}</p>
+        )}
+
+        <div className="flex-grow my-6">{children}</div>
+
+        <div className="mt-auto">
+          {hasCancelButton && (
+            <div className="flex justify-end space-x-2">
+              <button
+                onClick={onAccept}
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              >
+                Accept
+              </button>
+              <button
+                onClick={onClose}
+                className="px-4 py-2 bg-gray-600 rounded hover:bg-gray-400"
+              >
+                Exit
+              </button>
+            </div>
+          )}
+          {!hasCancelButton && (
+            <button
+              onClick={onAccept}
+              className="p-4 bg-blue-500 text-white rounded hover:bg-blue-600 w-full text-center"
+            >
+              Accept
+            </button>
+          )}
         </div>
       </div>
     </div>
