12714f113e4300406f5c1806446a356d6afc4f95
patricio-rosso <patriciosimonrosso@gmail.com>
2024-04-09 20:23:45 -0300

    transform FUNCTION into SP to also return exams

--- db/scripts/14 - FN-GetCourses.sql
+++ db/scripts/14 - FN-GetCourses.sql
@@ -2,18 +2,26 @@ USE studentcompass
 
 GO
 
-CREATE OR ALTER FUNCTION app.get_courses (
-    @student_id SMALLINT,
-    @career_plan_id TINYINT
-)
-RETURNS TABLE
+CREATE OR ALTER PROCEDURE app.get_courses
+@student_id SMALLINT,
+@career_plan_id TINYINT
 AS
-RETURN
-(
-    SELECT *
-	FROM app.course
-	WHERE student_id = @student_id
-	AND career_plan_id = @career_plan_id
-);
+BEGIN
+
+DECLARE @transversal_career_plan_id TINYINT = 0;
+
+-- courses
+SELECT *
+FROM app.course
+WHERE student_id = @student_id
+AND career_plan_id IN (@career_plan_id,@transversal_career_plan_id)
+
+-- exams
+SELECT ex.exam_id, ex.grade, ex.course_id
+FROM app.course_exam ex
+LEFT JOIN app.course c ON c.id = ex.course_id
+WHERE c.student_id = @student_id AND c.career_plan_id IN (@career_plan_id, @transversal_career_plan_id)
+
+END
 
 GO
