3210aa8277775ee0203c86d533d5641ca85d01a0
patricio-rosso <patriciosimonrosso@gmail.com>
2024-04-10 22:11:25 -0300

    rename SP for getting courses

--- db/scripts/14 - SP-GetCourses.sql
+++ db/scripts/14 - SP-GetCourses.sql
@@ -4,7 +4,8 @@ GO
 
 CREATE OR ALTER PROCEDURE app.get_courses
 @student_id SMALLINT,
-@career_plan_id TINYINT
+@career_plan_id TINYINT,
+@subject_code SMALLINT
 AS
 BEGIN
 
@@ -14,10 +15,12 @@ DECLARE @transversal_career_plan_id TINYINT = 0;
 SELECT *
 FROM app.course
 WHERE student_id = @student_id
+AND subject_code = @subject_code
 AND career_plan_id IN (@career_plan_id,@transversal_career_plan_id)
+ORDER BY year DESC, term_id DESC
 
 -- exams
-SELECT ex.exam_id, ex.grade, ex.course_id
+SELECT ex.exam_id, ex.grade, ex.taken_on, ex.course_id
 FROM app.course_exam ex
 LEFT JOIN app.course c ON c.id = ex.course_id
 WHERE c.student_id = @student_id AND c.career_plan_id IN (@career_plan_id, @transversal_career_plan_id)
