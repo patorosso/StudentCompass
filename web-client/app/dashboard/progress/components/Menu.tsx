d7ad6b7e1aaafd085654397cd4ddf874a99a2b9b
patricio-rosso <patriciosimonrosso@gmail.com>
2024-04-14 02:21:55 -0300

    fix menu rendering based on screen position and add smooth effect for displaying

--- web-client/app/dashboard/progress/components/Menu.tsx
+++ web-client/app/dashboard/progress/components/Menu.tsx
@@ -1,26 +1,80 @@
-"use client";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface MenuProps {
   onClick: (value: string) => void;
+  onClose?: () => void;
   values: string[];
   title?: string;
   maxWidth: string;
+  minWidth?: string;
+  optionalMargin?: string;
 }
 
-const Menu = ({ title, onClick, values, maxWidth }: MenuProps) => {
+const Menu = ({
+  title,
+  onClick,
+  onClose,
+  values,
+  optionalMargin,
+  maxWidth,
+  minWidth,
+}: MenuProps) => {
   const menuRef = useRef<HTMLDivElement | null>(null);
+  const [menuDirection, setMenuDirection] = useState("down");
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const checkMenuPosition = () => {
+      if (!menuRef.current) return;
+
+      const menuRect = menuRef.current.getBoundingClientRect();
+      const bottomSpace = window.innerHeight - menuRect.top - menuRect.height;
+      const topSpace = menuRect.top;
+
+      if (bottomSpace < 200 && topSpace > 200) {
+        // Assuming the height of the menu is around 120px
+        setMenuDirection("up");
+      } else {
+        setMenuDirection("down");
+      }
+
+      setIsReady(true); // Ensure the menu is ready to be transitioned in
+    };
+
+    // Delay the check slightly to ensure the menu is in the DOM
+    setTimeout(checkMenuPosition, 10);
+    window.addEventListener("resize", checkMenuPosition);
+
+    return () => {
+      window.removeEventListener("resize", checkMenuPosition);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        onClose?.();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [onClose]);
 
   return (
     <div
-      className={`absolute origin-center mt-10 ${maxWidth} rounded-sm shadow-2xl bg-white ring-1 ring-black ring-opacity-5  divide-gray-100 z-50`}
-      role="menu"
-      aria-orientation="vertical"
-      aria-labelledby="menu-button"
-      tabIndex={-1}
+      className={`absolute ${
+        menuDirection === "up" ? "bottom-full mb-[5px]" : "top-full mt-[10px]"
+      }
+        origin-center ${maxWidth} ${minWidth} ${optionalMargin} rounded-md shadow-2xl bg-slate-200 dark:bg-gray-500 divide-gray-100 z-20 transition-all duration-350 ease-in-out text-black dark:text-white`}
+      style={{
+        transform: isReady ? "scaleY(1)" : "scaleY(0)",
+        transformOrigin: menuDirection === "up" ? "bottom" : "top",
+        maxHeight: isReady ? "200px" : "0",
+      }}
       ref={menuRef}
     >
-      <div className="py-1 max-h-52 overflow-y-auto" role="none">
+      <div className="max-h-52 overflow-y-auto">
         {title && (
           <div className="flex border-b-2 border-slate-300 px-4">
             <p className="text-gray-700 block py-2 text-md">{title}</p>
@@ -29,10 +83,11 @@ const Menu = ({ title, onClick, values, maxWidth }: MenuProps) => {
         {values.map((value) => (
           <button
             key={value}
-            className={`block px-4 py-2 text-md w-full text-center hover:bg-gray-300`}
-            role="menuitem"
-            tabIndex={-1}
-            onClick={() => onClick(value)}
+            className="block px-4 py-2 text-md w-full text-center hover:bg-gray-300 dark:hover:bg-gray-400"
+            onClick={(event) => {
+              onClick(value);
+              onClose?.();
+            }}
           >
             {value}
           </button>
