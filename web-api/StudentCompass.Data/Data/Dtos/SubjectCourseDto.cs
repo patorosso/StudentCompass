c527f85f191fc427cb15695f39e8344656e7c67a
patricio-rosso <patriciosimonrosso@gmail.com>
2024-05-05 02:26:54 -0300

    refactor models and DTOs

--- web-api/StudentCompass.Data/Data/Dtos/SubjectCourseDto.cs
+++ web-api/StudentCompass.Data/Data/Dtos/SubjectCourseDto.cs
@@ -19,6 +19,20 @@
         public string Status { get; set; } = null!;
         public byte CareerPlanId { get; set; }
         public byte? FinalGrade { get; set; }
+    }
 
+    public class SubjectCourseDto
+    {
+        public short Code { get; set; }
+        public string Description { get; set; } = null!;
+        public byte WeeklyHours { get; set; }
+        public byte YearLevel { get; set; }
+        public bool IsOptional { get; set; }
+        public bool IsElective { get; set; }
+        public bool IsAnnual { get; set; }
+        public string Status { get; set; } = null!;
+        public byte? FinalGrade { get; set; }
+        public int? CourseId { get; set; }
+        public byte CareerPlanId { get; set; }
     }
 }
