e3814252fad609d3f1133c94342298ef244278e5
patricio-rosso <patriciosimonrosso@gmail.com>
2024-11-20 20:39:48 -0300

    reorder main and app so that main has all wrappers and functionalities for the app

--- studentcompass.client/src/App.tsx
+++ studentcompass.client/src/App.tsx
@@ -1,21 +1,16 @@
 import "./App.css";
-import { theme } from "./utils/themes";
+import React from "react";
+import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
-import { ToastContainer } from "react-toastify";
-import { Box, ThemeProvider } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Navbar />
-        <Box className={boxClassName} sx={boxStyle}>
-          <p>Content</p>
-        </Box>
-        <ToastContainer style={toastStyle} />
-      </ThemeProvider>
-    </BrowserRouter>
+    <React.Fragment>
+      <Navbar />
+      <Box className={boxClassName} sx={boxStyle}>
+        <p>Content</p>
+      </Box>
+    </React.Fragment>
   );
 }
 
@@ -31,10 +26,4 @@ const boxStyle = {
   overflow: "auto",
   flexDirection: "column",
   minHeight: "calc(100vh - 5rem)",
-};
-
-const toastStyle = {
-  maxWidth: 700,
-  marginTop: 67,
-  width: "auto",
 };
--- studentcompass.client/src/main.tsx
+++ studentcompass.client/src/main.tsx
@@ -1,5 +1,24 @@
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { theme } from "./utils/themes";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@mui/material";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// ------------ Styles ------------
+
+const toastStyle = {
+  maxWidth: 700,
+  marginTop: 67,
+  width: "auto",
+};
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+      <ToastContainer style={toastStyle} />
+    </ThemeProvider>
+  </BrowserRouter>
+);
